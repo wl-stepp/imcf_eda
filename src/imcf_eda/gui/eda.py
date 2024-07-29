cf9d88e5d629ece22eb0870e015c4b8fa9a0ce5f
Willi L Stepp <willi.stepp@epfl.ch>
2024-07-29 11:32:41 +0200

    eda gui advance

--- src/imcf_eda/gui/eda.py
+++ src/imcf_eda/gui/eda.py
@@ -1,10 +1,94 @@
-from qtpy.QtWidgets import QTabWidget, QVBoxLayout, QHBoxLayout, QPushButton
+from qtpy.QtWidgets import QTabWidget, QVBoxLayout, QHBoxLayout, QPushButton, QWidget
 from qtpy.QtCore import Signal
+from qtpy.QtGui import QFont
+
 from imcf_eda.gui._qt_classes import QWidgetRestore, set_eda, set_dark
 from imcf_eda.gui.overview import Overview
 from pymmcore_widgets import  MDAWidget
 from pymmcore_widgets.mda._save_widget import SaveGroupBox
 from pymmcore_plus import CMMCorePlus
+from imcf_eda.model import OverviewSettings, ScanSettings, AcquisitionSettings, AnalyserSettings
+
+
+class OverviewGUI(QWidget):
+    def __init__(self, mmc: CMMCorePlus):
+        super().__init__()
+        self.lay = QVBoxLayout()
+        self.setLayout(self.lay)
+        self.mda = MDAWidget(mmcore=mmc)
+        self.mda.save_info.hide()
+        self.mda.control_btns.hide()
+        self.mda.tab_wdg.setTabEnabled(0, False)
+        self.mda.tab_wdg._cboxes[0].hide() # time
+        self.mda.tab_wdg.setTabEnabled(1, False)
+        self.mda.tab_wdg._cboxes[1].hide() # position
+        self.mda.tab_wdg.setTabEnabled(3, False)
+        self.mda.tab_wdg._cboxes[3].hide() # z
+        self.mda.setWindowTitle("Overview")
+        self.lay.addWidget(self.mda)
+
+        self.settings = OverviewSettings()
+        self.lay.addWidget(self.settings.gui.native)
+
+        self.button = QPushButton("⏵ Run Overview")
+        self.button.setFont(QFont('Times', 16))
+        self.lay.addWidget(self.button)
+
+class ScanGUI(QWidget):
+    def __init__(self, mmc:CMMCorePlus):
+        super().__init__()
+        self.lay = QVBoxLayout()
+        self.setLayout(self.lay)
+
+        self.mda = MDAWidget(mmcore=mmc)
+        self.mda.save_info.hide()
+        self.mda.control_btns.hide()
+        self.mda.setWindowTitle("Scan")
+        self.mda.tab_wdg.setTabEnabled(0, False)
+        self.mda.tab_wdg._cboxes[0].hide() # time
+        self.lay.addWidget(self.mda)
+
+        self.settings = ScanSettings()
+        self.lay.addWidget(self.settings.gui.native)
+
+        self.scan_btn = QPushButton("⏵ Scan Only")
+        self.scan_btn.setFont(QFont('Times', 16))
+        self.scan_acq_btn = QPushButton("⏵ DualScan")
+        self.scan_acq_btn.setFont(QFont('Times', 16))
+        self.btn_lay = QHBoxLayout()
+        self.btn_lay.addWidget(self.scan_btn)
+        self.btn_lay.addWidget(self.scan_acq_btn)
+        self.lay.addLayout(self.btn_lay)
+
+class AcquisitionGUI(QWidget):
+    def __init__(self, mmc:CMMCorePlus):
+        super().__init__()
+        self.lay = QVBoxLayout()
+        self.setLayout(self.lay)
+
+        self.mda = MDAWidget(mmcore=mmc)
+        self.mda.save_info.hide()
+        self.mda.control_btns.hide()
+        self.mda.setWindowTitle("Acquisition")
+        self.mda.tab_wdg.setTabEnabled(0, False)
+        self.mda.tab_wdg._cboxes[0].hide() # time
+        self.lay.addWidget(self.mda)
+
+        self.analyser = AnalyserSettings()
+        self.lay.addWidget(self.analyser.gui.native)
+
+        self.settings = AcquisitionSettings()
+        self.lay.addWidget(self.settings.gui.native)
+
+        self.acq_btn = QPushButton("⏵ Acquire")
+        self.acq_btn.setFont(QFont('Times', 16))
+        self.lay.addWidget(self.acq_btn)
+
+        stylesheet = """
+        QTabBar::tab:selected {background: blue;}
+        QTabWidget>QWidget>QWidget{background: blue;}
+        """
+
 
 
 class EDAGUI(QWidgetRestore):
@@ -12,66 +96,38 @@ class EDAGUI(QWidgetRestore):
         super().__init__(parent)
 
         self.mmc = mmc
-
         self.tabs = QTabWidget()
         self.save_info = SaveGroupBox(parent=self)
 
-        self.overview = MDAWidget(mmcore=mmc)
-        self.overview.save_info.hide()
-        self.overview.control_btns.hide()
-        self.overview.tab_wdg.setTabEnabled(0, False)
-        self.overview.tab_wdg._cboxes[0].hide() # time
-        self.overview.tab_wdg.setTabEnabled(1, False)
-        self.overview.tab_wdg._cboxes[1].hide() # position
-        self.overview.tab_wdg.setTabEnabled(3, False)
-        self.overview.tab_wdg._cboxes[3].hide() # z
-        self.overview.setWindowTitle("Overview")
+        self.overview = OverviewGUI(mmc)
+        self.overview.button.pressed.connect(self.run_overview)
 
-        self.mda_1 = MDAWidget(mmcore=mmc)
-        self.mda_1.save_info.hide()
-        self.mda_1.control_btns.hide()
-        self.mda_1.setWindowTitle("Scan")
-        self.mda_1.tab_wdg.setTabEnabled(0, False)
-        self.mda_1.tab_wdg._cboxes[0].hide() # time
+        self.scan = ScanGUI(mmc)
 
-        self.mda_2 = MDAWidget(mmcore=mmc)
-        self.mda_2.save_info.hide()
-        self.mda_2.control_btns.hide()
-        self.mda_2.setWindowTitle("Acquisition")
-        set_eda(self.mda_2)
-        self.mda_2.tab_wdg.setTabEnabled(0, False)
-        self.mda_2.tab_wdg._cboxes[0].hide() # time
-
-        self.overview_btn = QPushButton("Overview")
-        self.overview_btn.pressed.connect(self.run_overview)
-        self.scan_btn = QPushButton("Scan")
-        self.acquisition_btn = QPushButton("Acquisition")
-        self.btn_layout = QHBoxLayout()
-        self.btn_layout.addWidget(self.overview_btn)
-        self.btn_layout.addWidget(self.scan_btn)
-        self.btn_layout.addWidget(self.acquisition_btn)
+        self.acquisition = AcquisitionGUI(mmc)
 
         self.tabs.addTab(self.overview, "Overview")
-        self.tabs.addTab(self.mda_1, "Scan")
-        self.tabs.addTab(self.mda_2, "Acquisition")
+        self.tabs.addTab(self.scan, "Scan")
+        self.tabs.addTab(self.acquisition, "Acquisition")
+
 
         self.lay = QVBoxLayout()
         self.setLayout(self.lay)
         self.lay.addWidget(self.save_info)
         self.lay.addWidget(self.tabs)
-        self.lay.addLayout(self.btn_layout)
 
     def run_overview(self):
-        overview_mda = self.overview.value()
+        overview_mda = self.overview.mda.value()
+        #TODO make sure obj is set
         self.mmc.run_mda(overview_mda)
         self.fov_select = QOverview()
         self.fov_select.new_fovs.connect(self.rcv_fovs)
 
     def rcv_fovs(self, fovs):
         print("FOVs received in EDA GUI")
-        scan_mda = self.mda_1.value()
-        scan_mda = scan_mda.replace(stage_positions = fovs)
-        self.mda_1.setValue(scan_mda)
+        scan_mda = self.scan.mda.value()
+        scan_mda = self.scan.mda.replace(stage_positions = fovs)
+        self.scan.mda.setValue(scan_mda)
 
 
 class QOverview(QWidgetRestore):
--- src/imcf_eda/model.py
+++ src/imcf_eda/model.py
@@ -27,7 +27,7 @@ SETTINGS = {
     "save": "F:/data/eda_data_027/",
 
     "objective_1": "5-Plan Apo 60x NA 1.42 Oil",
-    "analyser": {"threshold": 0.1, "closing_kernel": (3, 3), "channel": "4-Cy5", 
+    "analyser": {"threshold": 0.1, "closing_kernel": (3, 3), "channel": "4-Cy5",
                  "model_path": "F:/imcf_eda/models/unet2d_vish_v4/keras_weights.hdf5"},
     "sequence_1": useq.MDASequence(
             stage_positions = [(0, 0)], # This will be overwritten with the current position
@@ -49,3 +49,54 @@ SETTINGS = {
     "autofocus": False,
 
 }
+
+
+from magicgui.experimental import guiclass
+
+from typing import Annotated, Literal
+OBJECTIVES = Literal["5-Plan Apo 60x NA 1.42 Oil", "6-Plan Apo 100x NA 1.45 Oil"]
+
+@guiclass
+class OverviewSettings:
+    objective: str = "4x"
+
+@guiclass
+class ScanSettings:
+    objective: OBJECTIVES = "5-Plan Apo 60x NA 1.42 Oil"
+
+@guiclass
+class AnalyserSettings:
+    threshold: float = 0.1
+    closing_kernel: int = 3
+    channel: str = "4-Cy5"
+    model_path: str = "F:/imcf_eda/models/unet2d_vish_v4/keras_weights.hdf5"
+
+@guiclass
+class AcquisitionSettings:
+    min_border_distance: float = 1.
+    z_offset: float = 4.875
+    x_offset: Annotated[float, {'widget_type': "LineEdit"}] = -45.
+    y_offset: Annotated[float, {'widget_type': "LineEdit"}] = -11.
+    pixel_size_config: str = "100x" #('100x', '10x', '25x', '40x', '4x', '60x')
+    objective: OBJECTIVES = "6-Plan Apo 100x NA 1.45 Oil"
+    autofocus: bool = False
+
+
+
+# @dataclass
+@guiclass
+class SETTINGS:
+
+
+    mm_config: str = "C:/Program Files/Micro-Manager-2.0.3_June24/CSU-W1C_4dualcam_piezo.cfg"
+    save: str = "F:/data/eda_data_027/"
+
+
+
+
+if __name__ == "__main__":
+    from qtpy.QtWidgets import QApplication
+    app = QApplication([])
+    obj = SETTINGS()
+    obj.gui.show()
+    app.exec_()
